99a4847d5c97ee6983e45e805cf92b93ab74bb09
Ethan Moffat <ethan@moffat.io>
2023-07-11 23:11:07 -0700

    Add doc comments for limit constants

--- pkg/eolib/data/limits.go
+++ pkg/eolib/data/limits.go
@@ -1,8 +1,15 @@
 package data
 
 const (
-	CHAR_MAX  int = 253
+	// CHAR_MAX represents the maximum value of an EO char (1-byte encoded integer)
+	CHAR_MAX int = 253
+
+	// SHORT_MAX represents the maximum value of an EO short (2-byte encoded integer)
 	SHORT_MAX int = CHAR_MAX * CHAR_MAX
+
+	// THREE_MAX represents the maximum value of an EO three (3-byte encoded integer)
 	THREE_MAX int = CHAR_MAX * CHAR_MAX * CHAR_MAX
-	INT_MAX   int = SHORT_MAX * SHORT_MAX
+
+	// INT_MAX represents the maximum value of an EO int (4-byte encoded integer)
+	INT_MAX int = SHORT_MAX * SHORT_MAX
 )
