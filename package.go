a3b397f2cc2dbfafa7322f190be075cffe3655ea
Ethan Moffat <ethan@moffat.io>
2023-11-07 16:38:09 -0800

    Add top-level package docs

---
+++ package.go
@@ -0,0 +1,4 @@
+// eolib-go is the top-level directory for the eolib-go go repository.
+//
+// More information is available in the eolib package: [pkg/github.com/ethanmoffat/eolib-go/pkg/eolib]
+package eolibgo
---
+++ pkg/eolib/package.go
@@ -0,0 +1,4 @@
+// eolib is the core library for writing Endless Online applications using the Go programming language.
+//
+// More details are available in the package subdirectories.
+package eolib
