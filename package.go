b03038274bc74c2209b8eceb48c8f40a99d20c19
Ethan Moffat <ethan@moffat.io>
2023-11-07 20:55:16 -0800

    Add top-level package docs (#11)
    * Add top-level package docs
    
    * Improve package doc summaries

---
+++ package.go
@@ -0,0 +1,4 @@
+// eolibgo is the top-level package for the Go implementation of eolib.
+//
+// More details are available in the eolib subdirectory: [pkg/github.com/ethanmoffat/eolib-go/pkg/eolib]
+package eolibgo
